f5a0be4f0da5c5dfb2149352c22e50d3a75dea94
ADHIKARI Nilkanta <nilkanta.dasadhikari@soprasteria.com>
2026-04-25 12:59:47 +0530

    [UC-0a]Fix API KEY: dynamic ENV API key -> hard code api key

--- uc-0a/classifier.py
+++ uc-0a/classifier.py
@@ -5,9 +5,9 @@ import os
 import google.generativeai as genai
 
 # Read API key from environment
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = "AIzaSyDl4GvantRDRQFDA1BGTWG_elIdzLUANEI" ##os.environ.get("GEMINI_API_KEY")
 if api_key:
-    genai.configure(api_key="AIzaSyDl4GvantRDRQFDA1BGTWG_elIdzLUANEI")
+    genai.configure(api_key=api_key)
 
 generation_config = {
   "temperature": 0.0,
